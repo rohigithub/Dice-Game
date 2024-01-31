1cff595d6fa3e40402f2e2c57d837bb3f295288f
rohigithub <champsrohit19@gmail.com>
2024-01-31 12:22:19 +0530

    project commit-1

---
+++ src/app/dice/dice.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dice',
+  standalone: true,
+  imports: [],
+  templateUrl: './dice.component.html',
+  styleUrl: './dice.component.scss'
+})
+export class DiceComponent {
+  dice1:number=0;
+  dice2:number=0;
+  max=6;
+  min=1;
+  
+  rollDice()
+  {
+     this.dice1 = Math.floor(Math.random()* (this.max - this.min + 1)) + this.min;
+     this.dice2 = Math.floor(Math.random()* (this.max - this.min + 1)) + this.min;
+  }
+}
